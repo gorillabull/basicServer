8f41f85a4cb5bc04f7643aa1b3f7b71c20330ede
gorilla <thisisntlongonlytomakelifehard@gmail.com>
2020-05-28 17:36:47 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var http = require('http'); // 1 - Import Node.js core module
+
+var server = http.createServer(function (req, res) {   // 2 - creating server
+
+    // set response header
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write("hello");
+    res.end();
+
+});
+
+server.listen(5353); //3 - listen for any incoming requests
+
+console.log('Node.js web server at port 5000 is running..')
