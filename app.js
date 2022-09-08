89559112bb26c5958115a46f03c15feb9bde4eeb
Replit user <>
2022-09-08 21:53:28 +0000

    from replit

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var http = require('http'); // 1 - Import Node.js core module
 
-var server = http.createServer(function (req, res) {   // 2 - creating server
+var server = http.createServer(function(req, res) {   // 2 - creating server
 
-    // set response header
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write("hello");
-    res.end();
+  // set response header
+  //replit edits
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write("hello");
+  res.end();
 
 });
 
